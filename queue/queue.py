d28aa09bea0870882e1910309227fa6593858d4f
Katrina Roaix <kroaix@gmail.com>
2020-05-26 16:56:52 -0400

    array data structure for queue

--- queue/queue.py
+++ queue/queue.py
@@ -9,20 +9,35 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
+
+   Better memory usage 
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+import sys
+
+sys.path.append('../doubly_linked_list')
+
+from doubly_linked_list import DoublyLinkedList
+# Using an array data structure for storage: 
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+
     def __len__(self):
-        pass
+        return self.size
 
+#enqueue - getting in line
+#first in, first out
     def enqueue(self, value):
-        pass
+        self.storage.insert(0, value)
+        self.size += 1
 
+#dequeue - getting out of line
     def dequeue(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop()
+        return
